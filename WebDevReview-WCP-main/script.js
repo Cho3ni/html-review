d7c040ba562ae26a4a559cc29e4b0d4d1fab496c
Cho3ni <135648772+Cho3ni@users.noreply.github.com>
2024-09-30 10:54:06 -0400

    Add files via upload

---
+++ WebDevReview-WCP-main/script.js
@@ -0,0 +1,11 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed!';
+});
+
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText1').innerHTML = 'Football';
+});
+
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText2').innerHTML = 'I am actually full';
+});
